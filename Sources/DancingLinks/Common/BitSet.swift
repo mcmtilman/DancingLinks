f0e35951b2375b1654393664706314171c0cdb3b
Michel Tilman <mtilman@iMacKaby.local>
2019-06-01 20:49:43 +0200

    Updated comments

--- Sources/DancingLinks/Common/BitSet.swift
+++ Sources/DancingLinks/Common/BitSet.swift
@@ -15,10 +15,10 @@ public struct BitSet: SetAlgebra {
     
     // MARK: Constants
     
-    /// Answer the maximum supported value.
+    /// The maximum supported value.
     public static let max = Int.bitWidth - 1
     
-    /// Answer the minimum supported value.
+    /// The minimum supported value.
     public static let min = 0
     
     // MARK: Private stored properties
@@ -33,19 +33,19 @@ public struct BitSet: SetAlgebra {
         value.nonzeroBitCount
     }
     
-    /// Answer true if the set is empty.
+    /// True if the set is empty, false otherwise.
     public var isEmpty: Bool {
         value == 0
     }
     
     // MARK: Initializing
     
-    /// Initialize an empty set.
+    /// Initializes an empty set.
     public init() {
         self.init(value: 0)
     }
     
-    /// Initialize the set with given sequence of elements.
+    /// Initializes the set with given sequence of elements.
     /// Asserts that each element is inside the range.
     public init<S>(_ sequence: S) where S: Sequence, S.Element == Int {
         var value = 0
@@ -59,7 +59,7 @@ public struct BitSet: SetAlgebra {
         self.init(value: value)
     }
     
-    /// Initialize the set with given elements.
+    /// Initializes the set with given elements.
     /// Asserts that each element is inside the range.
     public init(_ elements: Int...) {
         self.init(elements)
@@ -67,7 +67,7 @@ public struct BitSet: SetAlgebra {
     
     // MARK: Testing
     
-    /// Answer if the element is in the set.
+    /// Returns true if the element is in the set, false otherwise.
     /// Asserts that the element is inside the range.
     public func contains(_ element: Int) -> Bool {
         assert(element >= Self.min && element <= Self.max)
@@ -77,8 +77,9 @@ public struct BitSet: SetAlgebra {
     
     // MARK: Element operations
     
-    /// Insert the element in the set. Answer if the element was inserted.
+    /// Inserts the element in the set.
     /// Asserts that the element is inside the range.
+    /// - SeeAlso: SetAlgebra
     @discardableResult
     public mutating func insert(_ element: Int) -> (inserted: Bool, memberAfterInsert: Int) {
         assert(element >= Self.min && element <= Self.max)
@@ -89,8 +90,9 @@ public struct BitSet: SetAlgebra {
         return (inserted: newValue != value, memberAfterInsert: element)
     }
     
-    /// Remove the element from the set. Answer the element if removed.
+    /// Removes the element from the set.
     /// Asserts that the element is inside the range.
+    /// - SeeAlso: SetAlgebra
     @discardableResult
     public mutating func remove(_ element: Int) -> Int? {
         assert(element >= Self.min && element <= Self.max)
@@ -101,8 +103,9 @@ public struct BitSet: SetAlgebra {
         return newValue != value ? element : nil
     }
     
-    /// Insert the element in the set. Answer the element if added.
+    /// Inserts the element in the set. Answer the element if added.
     /// Asserts that the element is inside the range.
+    /// - SeeAlso: SetAlgebra
     @discardableResult
     public mutating func update(with element: Int) -> Int? {
         insert(element).inserted ? element : nil
@@ -110,73 +113,73 @@ public struct BitSet: SetAlgebra {
     
     // MARK: Mutating set operations
     
-    /// Keep the intersection with the other set.
+    /// Keeps the intersection with the other set.
     public mutating func formIntersection(_ other: BitSet) {
         self = intersection(other)
     }
     
-    /// Subtract the intersection with the other set.
+    /// Subtracts the intersection with the other set.
     public mutating func formSymmetricDifference(_ other: BitSet) {
         self = symmetricDifference(other)
     }
     
-    /// Extend this set with the other set.
+    /// Extends this set with the other set.
     mutating public func formUnion(_ other: BitSet) {
         self = union(other)
     }
     
-    /// Remove the other set from this set.
+    /// Removes the other set from this set.
     mutating public func subtract(_ other: BitSet) {
         self = subtracting(other)
     }
     
     // MARK: Set operations
     
-    /// Answer the intersection of both sets.
+    /// Returns the intersection of both sets.
     public func intersection(_ other: BitSet) -> BitSet {
         BitSet(value: value & other.value)
     }
     
-    /// Answer the this set minus the other set.
+    /// Returns this set minus the other set.
     public func subtracting(_ other: BitSet) -> BitSet {
         intersection(symmetricDifference(other))
     }
     
-    /// Answer the symmetric difference of both sets (union - intersection).
+    /// Returns the symmetric difference of both sets (union - intersection).
     public func symmetricDifference(_ other: BitSet) -> BitSet {
         BitSet(value: value ^ other.value)
     }
     
-    /// Answer the union of both sets.
+    /// Returns the union of both sets.
     public func union(_ other: BitSet) -> BitSet {
         BitSet(value: value | other.value)
     }
     
     // MARK: Set testing
     
-    /// Answer if this set is a strict subset of the other.
+    /// Returns true if this set is a strict subset of the other, false otherwise.
     func isStrictSubset(of other: BitSet) -> Bool {
         isSubset(of: other) && self != other
     }
     
-    /// Answer if this set is a subset of the other.
+    /// Returns true if this set is a subset of the other, false otherwise.
     public func isSubset(of other: BitSet) -> Bool {
         self == intersection(other)
     }
     
-    /// Answer if this set is a strict superset of the other.
+    /// Returns true if this set is a strict superset of the other, false otherwise.
     func isStrictSuperset(of other: BitSet) -> Bool {
         other.isStrictSubset(of: self)
     }
     
-    /// Answer if this set is a superset of the other.
+    /// Returns true if this set is a superset of the other, false otherwise.
     public func isSuperset(of other: BitSet) -> Bool {
         other.isSubset(of: self)
     }
     
     // MARK: Private initializing
     
-    // Initialize the set with given bits.
+    // Initializes the set with given bits.
     private init(value: Int) {
         self.value = value
     }
@@ -189,7 +192,7 @@ public struct BitSet: SetAlgebra {
  */
 extension BitSet: ExpressibleByArrayLiteral {
     
-    /// Initialize the set with a literal array of Int.
+    /// Initializes the set with a literal array of Int.
     /// Asserts that each element is inside the range.
     public init(arrayLiteral: Int...) {
         self.init(arrayLiteral)
@@ -209,19 +212,16 @@ extension BitSet: Sequence {
         
         // Bitset representation of successive elements (1, 2, 4, ...).
         // 'Incremented' on each iteration.
-        // If private we need an explicit initializer.
         fileprivate var bitMask = 1
         
         // Successive elements (0, 1, 2, ...).
         // Incremented on each iteration.
-        // If private we need an explicit initializer.
         fileprivate var element = 0
         
         // Value containing the bits yet to be processed.
-        // If private we need an explicit initializer.
         fileprivate var value: Int
         
-        /// Answer the next element in the set, or nil if we are at the end.
+        /// Returns the next element in the set, or nil if we are at the end.
         public mutating func next() -> Int? {
             while value != 0 {
                 defer {
@@ -243,7 +243,7 @@ extension BitSet: Sequence {
         
     }
     
-    /// Answer an iterator ignoring changes after creation.
+    /// Returns an iterator which ignores changes to this set after creation.
     public func makeIterator() -> BitSetIterator {
         BitSetIterator(value: value)
     }
