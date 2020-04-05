b115ef605e1472869f2067c802bcc029737799db
Michel Tilman <mtilman@acm.org>
2020-04-05 14:20:15 +0200

    Updated Common dependency with version.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["DancingLinks"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/mcmtilman/Common.git", .branch("master")),
+        .package(url: "https://github.com/mcmtilman/Common.git", from: "1.0.0"),
     ],
     targets: [
         .target(
