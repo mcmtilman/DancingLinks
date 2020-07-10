d464aee31ba623c88b5820b9b3ca7d3c87959269
Michel Tilman <mtilman@acm.org>
2020-07-10 11:19:08 +0200

    Updated dependency version. Added tag.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["DancingLinks"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/mcmtilman/Common.git", from: "1.0.0"),
+        .package(url: "https://github.com/mcmtilman/Common.git", from: "1.1.2"),
     ],
     targets: [
         .target(
