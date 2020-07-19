96b3698c1095566568af08e30373b30c8b265de6
Michel Tilman <mtilman@acm.org>
2020-07-19 15:01:43 +0200

    Fixed formatting of SudokuGenerator.

--- Sources/DancingLinks/Examples/Sudoku/SudokuGenerator.swift
+++ Sources/DancingLinks/Examples/Sudoku/SudokuGenerator.swift
@@ -22,33 +22,33 @@ public class RandomSudokuGenerator {
     
     /// Generates a random solution for sudokus with given dimensions
     public func generateSolution(rows: Int = 3, columns: Int = 3) -> Sudoku? {
-            let size = rows * columns, cells = size * size
-            var gridOptions = [BitSet](repeating: BitSet(1 ... size), count: size * 3)
-            var values = [Int?](repeating: nil, count: cells)
+        let size = rows * columns, cells = size * size
+        var gridOptions = [BitSet](repeating: BitSet(1 ... size), count: size * 3)
+        var values = [Int?](repeating: nil, count: cells)
+        
+        func generate(_ cell: Int) -> Sudoku? {
+            guard cell < cells else { return Sudoku(values: values, rows: rows, columns: columns) }
             
-            func generate(_ cell: Int) -> Sudoku? {
-                guard cell < cells else { return Sudoku(values: values, rows: rows, columns: columns) }
-                
-                let row = cell / size, temp = cell % size, column = temp + size, box = row / rows * rows + temp / columns + size * 2
-                let saved = (gridOptions[row], gridOptions[column], gridOptions[box])
-                var cellOptions = saved.0.intersection(saved.1).intersection(saved.2)
-                
-                while let value = cellOptions.randomElement() {
-                    values[cell] = value
-                    for i in [row, column, box] {
-                        gridOptions[i].remove(value)
-                    }
-                    if let solution = generate(cell + 1) {
-                        return solution
-                    }
-                    (gridOptions[row], gridOptions[column], gridOptions[box]) = saved
-                    cellOptions.remove(value)
+            let row = cell / size, temp = cell % size, column = temp + size, box = row / rows * rows + temp / columns + size * 2
+            let saved = (gridOptions[row], gridOptions[column], gridOptions[box])
+            var cellOptions = saved.0.intersection(saved.1).intersection(saved.2)
+            
+            while let value = cellOptions.randomElement() {
+                values[cell] = value
+                for i in [row, column, box] {
+                    gridOptions[i].remove(value)
                 }
-                
-                return nil
+                if let solution = generate(cell + 1) {
+                    return solution
+                }
+                (gridOptions[row], gridOptions[column], gridOptions[box]) = saved
+                cellOptions.remove(value)
             }
             
-            return generate(0)
+            return nil
         }
+        
+        return generate(0)
+    }
     
 }
